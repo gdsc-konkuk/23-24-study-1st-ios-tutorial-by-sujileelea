dee2880c8480e3be490ff44a28b26747e1a80407
ParkSeongGeun <phd0801@naver.com>
2023-10-28 00:02:00 +0900

    feat: EditMemberModal에서 Member 객체 정보 변경 시 뷰에 반영되도록 수정 style: EditMemberModal 코드 수정
    - EditMemberModal의 필드 초기 변수 값을 Member객체에 선언되어있는 값으로 초기화
    - EditMemberModal 내 Rectangle을 VStack으로 감싸 코드 의미 해석이 빠르도록 수정

--- gdsc-ios-tutorial-by-sujileelea/View/MemberCard.swift
+++ gdsc-ios-tutorial-by-sujileelea/View/MemberCard.swift
@@ -13,30 +13,31 @@ struct MemberCard: View {
     @State var showEditMemberModal: Bool = false;
     
     var infoList: [String] = ["MBTI", "생일", "좋아하는 색", "주량"]
-    var answers: [String] = ["ISFP", "000801", "하늘색", "1병"]
+    var answers: [String] = [Member().mbti, Member().birthday, Member().favoriteColor, Member().drinkingCapacity]
     
     var body: some View {
-        RoundedRectangle(cornerRadius: 40)
-            .stroke(Color.cyan.opacity(0.7), lineWidth: 1)
-            .frame(width: 340, height: 630)
-            .foregroundColor(.white)
-            .overlay {
-                VStack {
-                    // 수정 항목 추가
-                    HStack {
+        VStack {
+            RoundedRectangle(cornerRadius: 40)
+                .stroke(Color.cyan.opacity(0.7), lineWidth: 1)
+                .frame(width: 340, height: 630)
+                .foregroundColor(.white)
+                .overlay {
+                    VStack {
+                        // 수정 항목 추가
+                        HStack {
+                            Spacer()
+                            Button(action: {
+                                showEditMemberModal = true;
+                            }, label: {
+                                Image(systemName: "square.and.pencil")
+                                    .font(.system(size: 30))
+                                    .foregroundColor(.mainGray)
+                            })
+                        }
+                        .padding(.trailing, 30)
+                        .padding(.top, 30)
                         Spacer()
-                        Button(action: {
-                            showEditMemberModal = true;
-                        }, label: {
-                            Image(systemName: "square.and.pencil")
-                                .font(.system(size: 30))
-                                .foregroundColor(.mainGray)
-                        })
-                    }
-                    .padding(.trailing, 30)
-                    .padding(.top, 30)
-                    Spacer()
-                    Image("profile_image")
+                        Image("profile_image")
                             .resizable()
                             .scaledToFit()
                             .frame(width: 206, height: 206)
@@ -47,40 +48,41 @@ struct MemberCard: View {
                             }
                             .shadow(color: Color.cyan, radius: 1)
                             .padding(.bottom)
-                    VStack {
-                        // Kor Name
-                        Text("박성근")
-                        // Eng Name
-                        Text("seonggpa")
-                    }
-                    .font(.system(size: 20))
-                    RoundedRectangle(cornerRadius: 3)
-                        .frame(width: 110, height: 5)
-                        .foregroundColor(.cyan)
-                        .padding(.bottom, 40)
-                    HStack (spacing: 75){
-                        VStack(alignment: .leading){
-                            //Foreach
-                            // 각 item의 id를 정해주도록 할 수 있음.
-                            // Array 내부 요소가 가지고 있는 값 자체를 id로 가지게 해줌.
-                            // [MBTI, 생일, 색, 주량] 각각의 요소에 id를 부여
-                            // id 부여 안할 시에 랄지랄지 함 -> Identifiable 프로토콜을 만족안한다고 판단하기 때문.
-                            ForEach(infoList, id: \.self) {
-                                question in
-                                Text(question)
-                                    .fontWeight(.semibold)
+                        VStack {
+                            // Kor Name
+                            Text(Member().name)
+                            // Eng Name
+                            Text(Member().nickname)
+                        }
+                        .font(.system(size: 20))
+                        RoundedRectangle(cornerRadius: 3)
+                            .frame(width: 110, height: 5)
+                            .foregroundColor(.cyan)
+                            .padding(.bottom, 40)
+                        HStack (spacing: 75){
+                            VStack(alignment: .leading){
+                                //Foreach
+                                // 각 item의 id를 정해주도록 할 수 있음.
+                                // Array 내부 요소가 가지고 있는 값 자체를 id로 가지게 해줌.
+                                // [MBTI, 생일, 색, 주량] 각각의 요소에 id를 부여
+                                // id 부여 안할 시에 랄지랄지 함 -> Identifiable 프로토콜을 만족안한다고 판단하기 때문.
+                                ForEach(infoList, id: \.self) {
+                                    question in
+                                    Text(question)
+                                        .fontWeight(.semibold)
+                                }
+                            }
+                            VStack(alignment: .trailing) {
+                                ForEach(answers, id: \.self){
+                                    answer in
+                                    Text(answer)
+                                }
                             }
                         }
-                        VStack(alignment: .trailing) {
-                            ForEach(answers, id: \.self){
-                                answer in
-                                Text(answer)
-                            }
-                        }
+                        Spacer()
                     }
-                    Spacer()
                 }
-            }
+        }
         // showEditMemberModal
         .sheet(isPresented: $showEditMemberModal, content: {
             EditMemberModal();
