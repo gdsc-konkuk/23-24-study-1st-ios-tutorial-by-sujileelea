84242ba4b6d5a6b2fb8fccfe759662054cae22d7
ParkSeongGeun <phd0801@naver.com>
2023-10-27 21:34:39 +0900

    feat: AddMemberModal을 위한 뷰 생성(구체화X)
    - AddMemberModal.swift 생성

---
+++ gdsc-ios-tutorial-by-sujileelea/View/AddMemberModal.swift
@@ -0,0 +1,21 @@
+//
+//  MemberCard.swift
+//  gdsc-ios-tutorial-by-sujileelea
+//
+//  Created by Suji Lee on 10/12/23.
+//
+
+import SwiftUI
+
+
+struct AddMemberModal: View {
+    var body: some View {
+       Text("Hello World!")
+    }
+}
+
+#Preview {
+    MemberCard()
+}
+
+
